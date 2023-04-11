a6c134af97c6cc84effcc5a3517f7cd9d7efdc0b
alekparkhomenko <alekparkhomenko@ozon.ru>
2023-04-11 23:25:57 +0300

    update proto-gen

--- lib/src/common/data/random.dart
+++
@@ -1,7 +0,0 @@
-library random;
-
-export './generated/random.pb.dart';
-export './generated/random.pbenum.dart';
-export './generated/random.pbjson.dart';
-export './generated/random.pbgrpc.dart';
-
--- proto_gen.sh
+++ proto_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc -I C:/sasha/home_projects/ts-node-grpc/proto/ C:/sasha/home_projects/ts-node-grpc/proto/random.proto --dart_out=grpc:lib/src/common/data/generated
+protoc -I /Users/alekparkhomenko/home_projects/ts-node-grpc/proto /Users/alekparkhomenko/home_projects/ts-node-grpc/proto/shop.proto --plugin=protoc-gen-dart=$HOME/.pub-cache/bin/protoc-gen-dart --dart_out=grpc:lib/src/common/data/generated
