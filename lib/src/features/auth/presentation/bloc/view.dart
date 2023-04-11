539a0d2c0879b37c7b1bf2fbeda8e9a2eb526525
alekparkhomenko <alekparkhomenko@ozon.ru>
2023-04-11 22:58:04 +0300

    add auth form widget

--- lib/src/features/auth/presentation/bloc/view.dart
+++ lib/src/features/auth/presentation/bloc/view.dart
@@ -1,10 +1,150 @@
+import 'package:amazon_grpc/constants/global_vars.dart';
+import 'package:amazon_grpc/src/common/widgets/app_button.dart';
+import 'package:amazon_grpc/src/common/widgets/app_text_field.dart';
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+enum Auth {
+  signin,
+  signup,
+}
+
+class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
 
   @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  Auth _auth = Auth.signup;
+  final _signInFormKey = GlobalKey<FormState>();
+  final _signUpFormKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _nameController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: GlobalVariables.greyBackgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Welcome",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              ListTile(
+                tileColor: _auth == Auth.signup
+                  ? GlobalVariables.backgroundColor
+                  : GlobalVariables.greyBackgroundColor,
+                title: const Text(
+                  'Create Account',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signup,
+                  groupValue: _auth,
+                  onChanged: (value) {
+                    setState(() {
+                      _auth = value ?? Auth.signup;
+                    });
+                  },
+                ),
+              ),
+              if (_auth == Auth.signup)
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: GlobalVariables.backgroundColor,
+                  child: Form(
+                    key: _signUpFormKey,
+                    child: Column(
+                      children: [
+                        AppTextField(
+                          controller: _nameController,
+                          hintText: 'Name',
+                        ),
+                        SizedBox(height: 10,),
+                        AppTextField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                        ),
+                        SizedBox(height: 10,),
+                        AppTextField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                        ),
+                        const SizedBox(height: 10,),
+                        AppButton(
+                            text: 'Sign Up',
+                            onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ListTile(
+                tileColor: _auth == Auth.signin
+                    ? GlobalVariables.backgroundColor
+                    : GlobalVariables.greyBackgroundColor,
+                title: const Text(
+                  'Sign-In',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signin,
+                  groupValue: _auth,
+                  onChanged: (value) {
+                    setState(() {
+                      _auth = value ?? Auth.signin;
+                    });
+                  },
+                ),
+              ),
+              if (_auth == Auth.signin)
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: GlobalVariables.backgroundColor,
+                  child: Form(
+                    key: _signUpFormKey,
+                    child: Column(
+                      children: [
+                        AppTextField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                        ),
+                        SizedBox(height: 10,),
+                        AppTextField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                        ),
+                        const SizedBox(height: 10,),
+                        AppButton(
+                          text: 'Sign In',
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
